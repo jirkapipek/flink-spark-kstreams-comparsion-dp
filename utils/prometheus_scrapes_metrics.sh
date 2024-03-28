3bfa8ad5ab5ff0cdcb9ea224da06ffb14c79edd2
jiri.pipek <jpipek@trask.cz>
2024-03-28 09:32:44 +0100

    fix prometheus metrics

--- utils/prometheus_scrapes_metrics.sh
+++ utils/prometheus_scrapes_metrics.sh
@@ -1,3 +1,11 @@
-curl -G -fsS --data-urlencode 'query=avg((node_memory_MemTotal_bytes - node_memory_MemAvailable_bytes) / node_memory_MemTotal_bytes) * 100' --data-urlencode 'start=1711324800' --data-urlencode 'end=1711325098' --data-urlencode 'step=1s' 'http://virtualserver1:9090/api/v1/query_range'
+curl -G -fsS \
+--data-urlencode 'query=avg((node_memory_MemTotal_bytes - node_memory_MemAvailable_bytes) / node_memory_MemTotal_bytes) * 100' \ 
+--data-urlencode 'start=1711565903' \
+--data-urlencode 'end=1711485491' \
+--data-urlencode 'step=1s' 'http://virtualserver1:9090/api/v1/query_range' > input.json
 
-curl -G -fsS --data-urlencode 'query=(sum by (instance) (irate(node_cpu_seconds_total{mode!="idle"}[5s])) / on(instance) group_left sum by (instance) (irate(node_cpu_seconds_total[5s]))) * 100' --data-urlencode 'start=1711324800' --data-urlencode 'end=1711325098' --data-urlencode 'step=5s' 'http://virtualserver1:9090/api/v1/query_range'
+curl -G -fsS 
+--data-urlencode 'query=(sum by (instance) (irate(node_cpu_seconds_total{mode!="idle"}[5s])) / on(instance) group_left sum by (instance) (irate(node_cpu_seconds_total[5s]))) * 100' \
+--data-urlencode 'start=1711324800' \ 
+--data-urlencode 'end=1711325098' \ 
+--data-urlencode 'step=5s' 'http://virtualserver1:9090/api/v1/query_range' > input.json
